eaafd08c2a2c369d45f492f76be0eb9212d00094
Angelica Delmendo <anggedelmendo08@gmail.com>
2023-02-15 20:38:17 +0800

    creating the window

--- WordCorrection.py
+++ WordCorrection.py
@@ -10,3 +10,7 @@ layout = [  [sg.Text('Input Word'),sg.Input(key='-IN-')],
             [sg.Button('Corrected Word'), sg.Button('Speak'), sg.Button('Clear'), sg.Button('Exit',expand_y=True)],
             [sg.Multiline(disabled=True, key='-OUT-', size=(100,20))]
         ]
+window = sg.Window('Word Correction', layout)
+def clear_inputs():
+    for key in values:
+        window['-IN-'].update('')
