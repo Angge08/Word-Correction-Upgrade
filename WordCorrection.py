ca26e1a196fff728184b66caee11271060d5bf38
Angelica Delmendo <anggedelmendo08@gmail.com>
2023-02-15 20:37:02 +0800

    add colors and buttons

--- WordCorrection.py
+++ WordCorrection.py
@@ -5,3 +5,8 @@ import pyttsx3
 engine = pyttsx3.init()
 sg.theme('LightGreen6')
 sg.set_options(font='Verdana 15')
+
+layout = [  [sg.Text('Input Word'),sg.Input(key='-IN-')],
+            [sg.Button('Corrected Word'), sg.Button('Speak'), sg.Button('Clear'), sg.Button('Exit',expand_y=True)],
+            [sg.Multiline(disabled=True, key='-OUT-', size=(100,20))]
+        ]
