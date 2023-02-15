5cf13e146ad884930a1245f29361da112bf7859f
Angelica Delmendo <anggedelmendo08@gmail.com>
2023-02-15 20:30:33 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import PySimpleGUI as sg
+from textblob import TextBlob, Word
+import pyttsx3
+engine = pyttsx3.init()
+sg.theme('LightGreen6')
+sg.set_options(font='Verdana 15')
+
+# Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Text('Input Word'),sg.Input(key='-IN-')],
+            [sg.Button('Corrected Word'), sg.Button('Speak'), sg.Button('Clear'), sg.Button('Exit',expand_y=True)],
+            [sg.Multiline(disabled=True, key='-OUT-', size=(100,20))]
+        ]
+
+# Create the Window
+window = sg.Window('Word Correction', layout)
+def clear_inputs():
+    for key in values:
+        window['-IN-'].update('')
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED or 'Exit'):
+        break
+    if event == 'Corrected Word':
+        a = values ['-IN-']
+        b = Word(a)
+        window['-OUT-'].update(b.spellcheck())
+
+    if event == 'Clear':
+        clear_inputs()
+    if event == 'Speak':
+        say_word = a
+        engine.say(say_word)
+        engine.runAndWait()
+
+window.close()
